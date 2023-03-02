a2c3a4f90ab4e2802a4e615e7e3a8168e9e8073b
Twan47 <anthonynewell171@gmail.com>
2023-03-02 09:27:12 -0500

    added pizza type

--- script.py
+++ script.py
@@ -6,8 +6,11 @@ num_pizzas = (len(toppings))
 print(num_pizzas)
 print("We sell", (num_pizzas), "different kinds of pizza!")
 pizza_and_prices = [[2, "pepperoni"], [6, "pineapple"], [1, "cheese"], [3, "sausage"], [2, "olives"], [7, "anchovies"], [2, "mushrooms"]]
-print(pizza_and_prices)
+pizza_and_prices.insert[2, (2.5, "peppers")]
 pizza_and_prices.sort()
+pizza_and_prices.pop(7)
 cheapest_pizza = pizza_and_prices[0:1]
+priciest_pizza = pizza_and_prices[6:7]
 print(pizza_and_prices)
 print(cheapest_pizza)
+print(priciest_pizza)
