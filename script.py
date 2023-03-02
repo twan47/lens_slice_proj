e4c5363a032050bf19ba66980e700561d130998a
Twan47 <anthonynewell171@gmail.com>
2023-03-02 08:44:55 -0500

    added print string for number

--- script.py
+++ script.py
@@ -2,3 +2,6 @@ toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies"
 prices = [2, 6, 1, 3, 2, 7, 2]
 num_two_dollar_slices = prices.count(2)
 print(num_two_dollar_slices)
+num_pizzas = (len(toppings))
+print(num_pizzas)
+print("We sell", (num_pizzas), "different kinds of pizza!")
