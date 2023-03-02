7ccbd264b70a8cf587eabff4d1a965c4fa9f19f1
Twan47 <anthonynewell171@gmail.com>
2023-03-02 08:38:33 -0500

    started proj. added main lists for toppings and prices. also counted 2's

---
+++ script.py
@@ -0,0 +1,4 @@
+toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
+prices = [2, 6, 1, 3, 2, 7, 2]
+num_two_dollar_slices = prices.count(2)
+print(num_two_dollar_slices)
