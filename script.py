cdd931c9412f9d04169cc3f1ef091966a3c9db37
Twan47 <anthonynewell171@gmail.com>
2023-03-02 08:52:37 -0500

    added new two dimensional list

--- script.py
+++ script.py
@@ -5,3 +5,5 @@ print(num_two_dollar_slices)
 num_pizzas = (len(toppings))
 print(num_pizzas)
 print("We sell", (num_pizzas), "different kinds of pizza!")
+pizza_and_prices = [[2, "pepperonmi"], [6, "pineapple"], [1, "cheese"], [3, "sausage"], [2, "olives"],
+                    [7, "anchovies"], [2, "mushrooms"]]
