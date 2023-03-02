c527ece3b2e8762893d16050777efd128bdffb7a
Twan47 <anthonynewell171@gmail.com>
2023-03-02 09:05:30 -0500

    sorted by prices and added cheapest sliced

--- script.py
+++ script.py
@@ -5,5 +5,9 @@ print(num_two_dollar_slices)
 num_pizzas = (len(toppings))
 print(num_pizzas)
 print("We sell", (num_pizzas), "different kinds of pizza!")
-pizza_and_prices = [[2, "pepperonmi"], [6, "pineapple"], [1, "cheese"], [3, "sausage"], [2, "olives"],
-                    [7, "anchovies"], [2, "mushrooms"]]
+pizza_and_prices = [[2, "pepperoni"], [6, "pineapple"], [1, "cheese"], [3, "sausage"], [2, "olives"], [7, "anchovies"], [2, "mushrooms"]]
+print(pizza_and_prices)
+pizza_and_prices.sort()
+cheapest_pizza = pizza_and_prices[0:1]
+print(pizza_and_prices)
+print(cheapest_pizza)
